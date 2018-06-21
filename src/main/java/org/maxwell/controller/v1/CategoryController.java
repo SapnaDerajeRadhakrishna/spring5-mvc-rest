24740bd7d210799de13e6a9a8de58b5f060affd3
Sapna Deraje Radhakrishna <sapna.dr@gmail.com>
2018-06-21 12:04:19 -0700

    Using Spring5 annotations

--- src/main/java/org/maxwell/controller/v1/CategoryController.java
+++ src/main/java/org/maxwell/controller/v1/CategoryController.java
@@ -4,13 +4,13 @@ import org.maxwell.api.v1.model.CategoryDTO;
 import org.maxwell.api.v1.model.CatorgoryListDTO;
 import org.maxwell.services.CategoryService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(CategoryController.BASE_URL)
 public class CategoryController {
 
@@ -23,14 +23,14 @@ public class CategoryController {
 	}
 
 	@GetMapping
-	public ResponseEntity<CatorgoryListDTO> getallCatetories() {
-
-		return new ResponseEntity<CatorgoryListDTO>(new CatorgoryListDTO(categoryService.getAllCategories()),
-				HttpStatus.OK);
+	@ResponseStatus(HttpStatus.OK)
+	public CatorgoryListDTO getallCatetories() {
+		 return new CatorgoryListDTO(categoryService.getAllCategories());
 	}
 
 	@GetMapping("{name}")
-	public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name) {
-		return new ResponseEntity<CategoryDTO>(categoryService.getCategoryByName(name), HttpStatus.OK);
+	@ResponseStatus(HttpStatus.OK)
+	public CategoryDTO getCategoryByName(@PathVariable String name) {
+		 return categoryService.getCategoryByName(name);
 	}
 }
--- src/main/java/org/maxwell/controller/v1/CustomerController.java
+++ src/main/java/org/maxwell/controller/v1/CustomerController.java
@@ -4,8 +4,6 @@ import org.maxwell.api.v1.model.CustomerDTO;
 import org.maxwell.api.v1.model.CustomerListDTO;
 import org.maxwell.services.CustomerService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -14,12 +12,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(CustomerController.BASE_URL)
 public class CustomerController {
-	
-	 public static final String BASE_URL = "/api/v1/customers";
+
+	public static final String BASE_URL = "/api/v1/customers";
 
 	private final CustomerService customerService;
 
@@ -28,37 +28,40 @@ public class CustomerController {
 	}
 
 	@GetMapping
-	public ResponseEntity<CustomerListDTO> getallCustomers() {
-
-		return new ResponseEntity<CustomerListDTO>(new CustomerListDTO(customerService.getAllCustomers()),
-				HttpStatus.OK);
+	@ResponseStatus(HttpStatus.OK)
+	public CustomerListDTO getallCustomers() {
+		return new CustomerListDTO(customerService.getAllCustomers());
 	}
 
-	@GetMapping({"/{id}"})
-    public ResponseEntity<CustomerDTO> getCustomerById(@PathVariable Long id){
-        return new ResponseEntity<CustomerDTO>(customerService.getCustomerById(id), HttpStatus.OK);
-    }
+	@GetMapping({ "/{id}" })
+	@ResponseStatus(HttpStatus.OK)
+	public CustomerDTO getCustomerById(@PathVariable Long id) {
+		return customerService.getCustomerById(id);
+	}
 
 	@PostMapping
-	public ResponseEntity<CustomerDTO> createNewCustomer(@RequestBody CustomerDTO customerDTO) {
-		return new ResponseEntity<CustomerDTO>(customerService.createNewCustomer(customerDTO), HttpStatus.CREATED);
+	@ResponseStatus(HttpStatus.CREATED)
+	public CustomerDTO createNewCustomer(@RequestBody CustomerDTO customerDTO) {
+		return customerService.createNewCustomer(customerDTO);
 	}
 
-	
 	@PutMapping({ "/{id}" })
-	public ResponseEntity<CustomerDTO> updateCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO) {
-		return new ResponseEntity<CustomerDTO>(customerService.saveCustomerByDTO(id, customerDTO), HttpStatus.OK);
+	@ResponseStatus(HttpStatus.OK)
+	public CustomerDTO updateCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO) {
+		return customerService.saveCustomerByDTO(id, customerDTO);
 	}
-	
+
 	@PatchMapping({ "/{id}" })
-	public ResponseEntity<CustomerDTO> patchCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO) {
-		return new ResponseEntity<CustomerDTO>(customerService.patchCustomer(id, customerDTO), HttpStatus.OK);
+	@ResponseStatus(HttpStatus.OK)
+	public CustomerDTO patchCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO) {
+		return customerService.patchCustomer(id, customerDTO);
 	}
-	
-	@DeleteMapping({"/{id}"})
-    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id){
-        customerService.deleteCustomerById(id);
-        return new ResponseEntity<Void>(HttpStatus.OK);
-    }
-	
+
+	@DeleteMapping({ "/{id}" })
+	@ResponseStatus(HttpStatus.OK)
+	public void deleteCustomer(@PathVariable Long id) {
+		customerService.deleteCustomerById(id);
+
+	}
+
 }
